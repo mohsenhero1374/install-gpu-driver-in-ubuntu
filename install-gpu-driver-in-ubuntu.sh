d43ccdf8e8c3e3086d3e6f1f6173499849c1c5a6
mohsenhero1374 <109383404+mohsenhero1374@users.noreply.github.com>
2023-07-26 09:39:28 +0330

    Update install-gpu-driver-in-ubuntu.sh

--- install-gpu-driver-in-ubuntu.sh
+++ install-gpu-driver-in-ubuntu.sh
@@ -1,10 +1,10 @@
 
 #!/bin/bash
 
-    sudo lspci -vnn | grep -i nvid -B20 > /tmp/gpu-type
-
+if  grep 'NAME="Ubuntu"' /etc/os-release; then # in this line checking that operating system is ubuntu"
+    sudo lspci -vnn | grep -i nvid -B20 > /tmp/GPU-type
     sudo apt update && sudo apt install gcc -y && sudo apt install make -y
-    ftpaddress=172.20.91.30
+    ftpaddress=172.20.91.30 #local addresses for download gpu driver#
     if grep -iq "20f1" /tmp/gpu-type; then   #this line check gpu id in a100 graphics #
       sudo wget ftp://$ftpaddress/GPU/Linux_64-bit/Tesla/a100/NVIDIA-Linux-x86_64-515.48.07.run 2> /dev/null
       sudo chmod +x NVIDIA-Linux-x86_64-515.48.07.run
@@ -20,5 +20,7 @@
       sudo chmod +x NVIDIA-Linux-x86_64-515.65.01.run
       sudo ./NVIDIA-Linux-x86_64-515.65.01.run --ui=none -a -s -q
     fi
-    sudo nvidia-smi
-
+    sudo nvidia-smi #show that GPU driver is installed or not #
+else 
+    echo "This type of operating system is not related to Ubuntu."
+fi
